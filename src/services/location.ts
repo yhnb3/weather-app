f4f927a2bd2f6ec0efba6ad5e7c84a0744590d50
yhnb3 <60080270+yhnb3@users.noreply.github.com>
2022-06-04 18:19:52 +0900

    Feat : cors 해결 안되나?

--- src/services/location.ts
+++ src/services/location.ts
@@ -1,13 +1,13 @@
 import { axios } from 'hooks/worker'
 import { ILocationRes } from 'types/location'
 
-const PROXY = window.location.hostname === '' ? '' : '/proxy'
+// const PROXY = window.location.hostname === 'localhost' ? '' : '/proxy'
 
 const GEOCODING_BASE_URL = '/map-geocode/v2/geocode'
 
 export const getLocation = (params: string) =>
   axios
-    .get<ILocationRes>(`${PROXY}${GEOCODING_BASE_URL}`, {
+    .get<ILocationRes>(`${GEOCODING_BASE_URL}`, {
       headers: {
         'X-NCP-APIGW-API-KEY': String(process.env.REACT_APP_NAVER_SECRET_KEY),
         'X-NCP-APIGW-API-KEY-ID': String(process.env.REACT_APP_NAVER_API_KEY),
